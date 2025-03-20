c6362087310e4bdee522b92b7e0372a2f5c36ad4
Emilien Maillet <emaillet@c2r7p5.42lehavre.fr>
2025-03-20 04:59:27 +0100

    CPP00/EX00 OK

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaillet <emaillet@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 01:12:27 by emaillet          #+#    #+#             */
+/*   Updated: 2025/03/20 04:47:46 by emaillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+int	main(int argc, char *argv[])
+{
+	std::string s;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		s = argv[i];
+		std::transform(s.begin(), s.end(), s.begin(), toupper);
+		std::cout << s;
+	}
+	std::cout << std::endl;
+	return (1);
+}
