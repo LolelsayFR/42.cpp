d734dafe42c61ec847464094926ab58e0bc95e3a
LolelsayFR <emaillet@student.42lehavre.fr>
2025-06-12 05:44:20 +0200

    Ex 06 ok

---
+++ cpp01/ex06/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 02:18:39 by emaillet          #+#    #+#             */
+/*   Updated: 2025/06/12 03:44:57 by emaillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <string>
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public:
+		Harl();
+		~Harl();
+		void complain( std::string level );
+};
+
+#endif // HARL_HPP
---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 02:18:07 by emaillet          #+#    #+#             */
+/*   Updated: 2025/06/12 05:31:17 by emaillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (std::cout << "Insert one arg" << std::endl, ac);
+	Harl harl;
+	harl.complain(av[1]);
+	return (0);
+}
