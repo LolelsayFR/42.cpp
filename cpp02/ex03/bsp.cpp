ef3fd7206228a274d6c1414847f3f2b1550bdc19
LolelsayFR <emaillet@student.42lehavre.fr>
2025-07-23 08:50:51 +0200

    CPP02 ex03 WIP

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 07:26:46 by emaillet          #+#    #+#             */
-/*   Updated: 2025/07/23 08:42:42 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/07/23 08:50:46 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
 /*	Atention les points ne sont pas forcement alligner
 /*	Lien utile :
 /*		https://www.youtube.com/watch?v=kkucCUlyIUE&themeRefresh=1
-/*		https://algo.developpez.com/actu/133518/Determiner-si-un-point-est-dans-un-triangle-un-billet-de-blog-de-wiwaxia/
+/*		https://ifdesignelseart.com/?p=306
 /* ************************************************************************** */
