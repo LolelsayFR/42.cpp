46aaf8ff52ddcbb1d1dc680954b2aead2f26357e
LolelsayFR <emaillet@student.42lehavre.fr>
2025-07-25 03:54:25 +0200

    CPP03 temp give up

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 07:26:46 by emaillet          #+#    #+#             */
-/*   Updated: 2025/07/23 08:50:46 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/07/25 03:38:22 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
 /*	Algo de verification si dans un triangle en 3 points
 /*	OK si point est entre a et b, et que le point est entre a et c, same pour b et c 
 /*	Atention les points ne sont pas forcement alligner
-/*	Lien utile :
+/*	Liens utile :
 /*		https://www.youtube.com/watch?v=kkucCUlyIUE&themeRefresh=1
 /*		https://ifdesignelseart.com/?p=306
 /* ************************************************************************** */
