75d6cdaaa698f3f8aa0ff3fe7790bed88ea6a0fe
LolelsayFR <emaillet@student.42lehavre.fr>
2025-07-30 08:00:51 +0200

    CPP04 ex02 ok

---
+++ cpp04/ex02/Animal.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 00:35:35 by emaillet          #+#    #+#             */
+/*   Updated: 2025/07/30 06:47:43 by emaillet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+# include "Brain.hpp"
+
+# define RED	"\e[48;2;75;0;0;1m "
+# define GRN	"\e[48;2;0;75;0;1m "
+# define BLU	"\e[48;2;0;0;75;1m "
+# define YEL	"\e[48;2;100;100;0;1m "
+# define RES	" \e[0m"
+
+class Animal
+{
+protected:
+	std::string	type;
+public:
+	Animal();
+	Animal(const Animal& other);
+	Animal& operator=(const Animal& other);
+	virtual ~Animal();
+	std::string	getType(void) const;
+	virtual void	makeSound(void) const;
+};
+
+#endif // ANIMAL_HPP
