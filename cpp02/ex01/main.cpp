592e43099853ec9d1cb1667850c7e668b1d3fca0
LolelsayFR <emaillet@student.42lehavre.fr>
2025-07-31 07:21:54 +0200

    Better main ex01

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,29 +6,52 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 23:13:40 by emaillet          #+#    #+#             */
-/*   Updated: 2025/07/23 05:05:05 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/07/31 07:21:08 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
+void subject(void) {
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+}
+
+void    other(void) {
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+}
+
 int main( void ) {
-    Fixed a;
-    Fixed const b( 10 );
-    Fixed const c( 42.42f );
-    Fixed const d( b );
 
-    a = Fixed( 1234.4321f );
-
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-    return (0);
+	std::cout << "\n\e[48;2;0;0;155;1m Subject test \e[0m\n" << std::endl;
+	subject();
+	std::cout << "\n\e[48;2;0;0;155;1m My test \e[0m\n" << std::endl;
+	other();
+	std::cout << "\n\e[48;2;0;0;155;1m End \e[0m\n" << std::endl;
+	return (0);
 }
