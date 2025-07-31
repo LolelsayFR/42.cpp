3747ae7761c16ec34f971ee7701d3c74adcfdfba
LolelsayFR <emaillet@student.42lehavre.fr>
2025-07-31 10:07:21 +0200

    Start ex03 cpp4

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,17 +6,17 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 09:37:50 by emaillet          #+#    #+#             */
-/*   Updated: 2025/07/31 09:58:34 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/07/31 10:06:54 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MateriaSource.hpp"
+#include "IMateriaSource.hpp"
 #include "AMateria.hpp"
 #include "Character.hpp"
 #include "ICharacter.hpp"
-#include "IMateriaSource.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
-#include "MateriaSource.hpp"
 
 int	main() {
 	IMateriaSource* src = new MateriaSource();
