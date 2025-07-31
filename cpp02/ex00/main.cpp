9734c7648d985d89fe13a327c3052bfb04cd9121
LolelsayFR <emaillet@student.42lehavre.fr>
2025-07-31 07:23:21 +0200

    Better main ex00

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,14 +6,14 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 23:13:40 by emaillet          #+#    #+#             */
-/*   Updated: 2025/07/25 03:45:06 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/07/31 07:22:33 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
-int main( void ) {
+int subject( void ) {
     Fixed a;
     Fixed b( a );
     Fixed c;
@@ -23,4 +23,12 @@ int main( void ) {
     std::cout << c.getRawBits() << std::endl;
     a.setRawBits(0);
     return (0);
+}
+
+int main( void ) {
+
+	std::cout << "\n\e[48;2;0;0;155;1m Subject test \e[0m\n" << std::endl;
+	subject();
+	std::cout << "\n\e[48;2;0;0;155;1m End \e[0m\n" << std::endl;
+	return (0);
 }
