c238539a98d0f1672a446b65eed4ce20cd005d83
emaillet <emaillet@student.42lehavre.fr>
2025-10-24 11:33:58 +0200

    ADD | CPP6 ex01 ok ?

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:13:43 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/24 10:53:51 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/10/24 11:32:02 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 #include <cstdlib>
 #include <climits>
 #include <sstream>
-//itoa lib
 #include <cstdlib>
 #include <cstring>
-
+#include <cfloat>
 
 
 ScalarConverter::ScalarConverter(void) {}
@@ -35,11 +34,6 @@ bool isChar(std::string s) {
 	char*	stash = NULL;
 	double	value = std::strtod(s.c_str(), &stash);
 
-	//std::cout << "Value: " << value << std::endl;
-	//std::cout << "Stash: " << stash << std::endl;
-	//std::cout << "String: " << s << std::endl;
-	//std::cout << "Value round unit : " << value / 1 << std::endl;
-
 	if (s[1] == '\0')
 		return (true);
 	else if (isascii(value) && (std::string(stash).empty() || (stash[0] == 'f'	&& stash[1] == '\0')) && value == (int)value)
@@ -47,45 +41,43 @@ bool isChar(std::string s) {
 	return (false);
 }
 
-//bool isInt(std::string s) {
-//	return (false);
-//}
-
-//bool isFloat(std::string s) {
-//	return (false);
-//}
-
-//bool isDouble(std::string s) {
-//	return (false);
-//}
-
 void ScalarConverter::convert(char* str) {
+	char *stash;
+
 	{//CHAR CONVERSION
 		if (isChar(str) && ((str[1] == '\0' && !std::isprint(str[0])) || (str[1] != '\0' && !std::isprint(std::strtod(str, NULL)))))
-			std::cout << "Char: Non displayable" << std::endl;
+			std::cout << "Char   : Non displayable" << std::endl;
 		else if (isChar(str) && str[1] == '\0') 
-			std::cout << "Char: '" << static_cast<char>(str[0]) << "'" << std::endl;
+			std::cout << "Char   : '" << static_cast<char>(str[0]) << "'" << std::endl;
 		else if (isChar(str))
-			std::cout << "Char: '" << static_cast<char>(std::strtod(str, NULL) / 1) << "'" << std::endl;
+			std::cout << "Char   : '" << static_cast<char>(std::strtod(str, NULL) / 1) << "'" << std::endl;
 		else if (isChar(str) == false)
-			std::cout << "Char: impossible" << std::endl;
+			std::cout << "Char   : impossible" << std::endl;
 	}
 	{//INT CONVERSION
-		if (1)
-			std::cout << "Int: " << static_cast<int>(std::strtod(str, NULL) / 1) << std::endl;
-		else
-			std::cout << "Int: impossible" << std::endl;
+		long l = static_cast<long>(std::strtol(str, &stash, 10));
+		if (l < INT_MIN || l > INT_MAX)
+			std::cout << "Int    : impossible (out of range)" << std::endl;
+		else {
+			int i = static_cast<int>(std::strtol(str, &stash, 10));
+			if (stash[0] == '\0' || ((stash[0] == 'f') && stash[1] == '\0'))
+				std::cout << "Int    : " << i << std::endl;
+			else
+				std::cout << "Int    : impossible" << std::endl;
+		}
 	}
 	{//FLOAT CONVERSION
-		if (1)
-			std::cout << "Float: " << static_cast<float>(std::strtod(str, NULL)) << "f" << std::endl;
+		float f = static_cast<float>(std::strtof(str, &stash));
+		if (stash[0] == '\0' || (stash[0] == 'f' && stash[1] == '\0'))
+			std::cout << "Float  : " << f << 'f' << std::endl;
 		else
-			std::cout << "Float: impossible" << std::endl;
+			std::cout << "Float  : impossible" << std::endl;
 	}
 	{//DOUBLE CONVERSION
-		if (1)
-			std::cout << "Double: " << static_cast<double>(std::strtod(str, NULL)) << std::endl;
+		double d = static_cast<double>(std::strtod(str, &stash));
+		if (stash[0] == '\0' || (stash[0] == 'f' && stash[1] == '\0'))
+			std::cout << "Double : " << d << std::endl;
 		else
-			std::cout << "Double: impossible" << std::endl;
+			std::cout << "Double : impossible" << std::endl;
 	}
 }
