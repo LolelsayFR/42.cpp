2698a60071a1969691bf6c5327acfad2a8ae7d4c
emaillet <emaillet@student.42lehavre.fr>
2025-10-25 13:49:55 +0200

    CHANGE | CPP7 Add explicit template instantiation

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 11:58:23 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/25 13:43:27 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/10/25 13:49:25 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ void print(int const& i) {
 int main( void ) {
 	int intArray[] = {0, 1, 2, 3, 4};
 	std::cout << "💠Integer array:" << std::endl;
-	iter(intArray, 5, print);
+	iter<int>(intArray, 5, print); //explicit template instantiation
 
 	std::cout << std::endl;
 
 	std::string strArray[] = {"Hello", "World", "Bernard", "Chuck", "Norris"};
 	std::cout << "💠String array:" << std::endl;
-	iter(strArray, 5, print);
+	iter(strArray, 5, print); //implicit template instantiation
 
 	return 0;
 }
