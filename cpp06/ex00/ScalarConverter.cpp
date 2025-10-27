304dfdd52bacbfab8315e8dff0121be03b9ae5d3
LolelsayFR <emaillet@student.42lehavre.fr>
2025-10-27 08:37:20 +0100

    ADD | CPP6 fix int INF

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:13:43 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/25 09:03:49 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/10/27 08:35:15 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ void ScalarConverter::convert(char* str) {
 		if (l < INT_MIN || l > INT_MAX)
 			std::cout << "Int    : impossible (overflow)" << std::endl;
 		else {
-			int i = static_cast<int>(std::strtod(str, &stash));
-			if (stash[0] == '\0' || (stash[0] == 'f' && stash[1] == '\0'))
+			int i = static_cast<int>(std::strtol(str, &stash, 10));
+			if (stash[0] == '\0' || (stash[0] == 'f' && stash[1] == '\0') || (stash[0] == '.'))
 				std::cout << "Int    : " << i << std::endl;
 			else
 				std::cout << "Int    : impossible" << std::endl;
