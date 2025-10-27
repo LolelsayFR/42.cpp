33e878af7d70b840e8e25529fe06b15df2c9b9de
LolelsayFR <emaillet@student.42lehavre.fr>
2025-10-27 09:05:02 +0100

    ADD | CPP6 ex02 better main

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 10:56:45 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/27 08:44:48 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/10/27 09:01:38 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int main(void) {
 	Data		*data = new Data("Jean de La Fontaine", "The Fox and the Crow", "Once upon a time...");
 	Serializer	serializer;
 
-	std::cout << *data;
+	std::cout << *data; // Data print
 	uintptr_t serializedData = serializer.serialize(data);
-	data->content = "This content has been changed.";
-	std::cout << "Addresse : " << data << " or " << reinterpret_cast<uintptr_t>(data) << "\n";
-	std::cout << "Serialized Data (as uintptr_t): " << serializedData << "\n\n";
+	data->author = "Bernard";
 	Data *deserializedData = serializer.deserialize(serializedData);
-	std::cout << "Deserialized Addresse : " << deserializedData << " or " << (uintptr_t)deserializedData << "\n";
-	std::cout << *deserializedData;
+	std::cout << *deserializedData; // Data print deserialize
 
-	delete data;
+	delete deserializedData;
 	return (0);
 }
