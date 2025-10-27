5b8535091a6795731df6b7fe6754bece746d2c2e
LolelsayFR <emaillet@student.42lehavre.fr>
2025-10-27 08:45:29 +0100

    ADD | CPP6 fix c cast

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 10:56:45 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/25 10:28:02 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/10/27 08:44:48 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(void) {
 	std::cout << *data;
 	uintptr_t serializedData = serializer.serialize(data);
 	data->content = "This content has been changed.";
-	std::cout << "Addresse : " << data << " or " << (uintptr_t)data << "\n";
+	std::cout << "Addresse : " << data << " or " << reinterpret_cast<uintptr_t>(data) << "\n";
 	std::cout << "Serialized Data (as uintptr_t): " << serializedData << "\n\n";
 	Data *deserializedData = serializer.deserialize(serializedData);
 	std::cout << "Deserialized Addresse : " << deserializedData << " or " << (uintptr_t)deserializedData << "\n";
