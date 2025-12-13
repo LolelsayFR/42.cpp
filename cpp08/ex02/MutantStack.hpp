6c8d00b5ceaffbe2fa401dedb5077fa72f19171e
emaillet <emaillet@student.42lehavre.fr>
2025-12-13 21:55:31 +0100

    FIX: Ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,12 +6,15 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 16:34:08 by emaillet          #+#    #+#             */
-/*   Updated: 2025/10/25 16:49:11 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/12/13 21:54:00 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
 #include <stack>
-#include <iterator>
+#include <deque>
 
 //Stack is a container adaptor that gives the programmer the functionality of a stack - specifically, a LIFO (last-in, first-out) data structure.
 //By default, the underlying container to use is std::deque.
@@ -19,22 +22,24 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
 public:
-	MutantStack() : std::stack<T>() {}
-	MutantStack(const MutantStack& other) : std::stack<T>(other) {}
-	~MutantStack() {}
-	MutantStack& operator=(const MutantStack& other) {
-		std::stack<T>::operator=(other);
-		return *this;
-	}
+    // Orthodox Canonical Form
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    virtual ~MutantStack() {}
 
-	typedef typename std::stack<T>::container_type::iterator iterator;
-	//container_type is the type of the underlying container (e.g., std::deque)
+    MutantStack& operator=(const MutantStack& other) {
+        if (this != &other)
+            std::stack<T>::operator=(other);
+        return *this;
+    }
 
-	iterator begin() {
-		return this->c.begin(); //This.c is the underlying container of the stack
-	}
-	iterator end() {
-		return this->c.end(); //This.c is the underlying container of the stack
-	}
+    // Typedef pour l'itérateur (C++98 compliant)
+    typedef typename std::stack<T>::container_type::iterator iterator;
 
-};	
+    // Fonctions membres pour les itérateurs
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+
+};
+
+#endif
